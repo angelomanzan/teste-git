e366faa66c07b779295c3e5f5e5001a6f1199dea
root <root@ip-10-0-1-125.ec2.internal>
2021-02-08 15:28:47 +0000

    second commit

---
+++ config/config_db.php
@@ -0,0 +1,7 @@
+<?php
+class DB extends DBmysql {
+   public $dbhost     = 'db-mysql-hml.evolog.com.br';
+   public $dbuser     = 'teste';
+   public $dbpassword = 'teste';
+   public $dbdefault  = 'teste';
+}
